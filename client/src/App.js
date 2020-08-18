770360f63ab85e7ea604296f0a7c2588418bb5e4
ajg7 <ajgebara@gmail.com>
2020-08-17 21:27:28 -0500

    rendered movie cards to the screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { Route } from "react-router-dom";
 import axios from 'axios';
+import MovieList from "../src/Movies/MovieList.js";
+import Movie from "../src/Movies/Movie.js";
 
 import SavedList from './Movies/SavedList';
 
@@ -28,7 +31,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+      <Route path="/">
+        <MovieList movies={movieList} />
+      </Route>
+      <Route path="/movies/:id">
+        <Movie />
+      </Route>
+      </div>
     </div>
   );
 };
